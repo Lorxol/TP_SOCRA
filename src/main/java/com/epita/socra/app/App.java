e3f326c46bf1b62ef7890a22f9abcf4dbd86b025
Vianney Delaboudiniere <vianney.delaboudiniere@epita.fr>
2020-05-28 16:29:14 +0200

    [ADD] added given files

---
+++ src/main/java/com/epita/socra/app/App.java
@@ -0,0 +1,36 @@
+package com.epita.socra.app;
+
+import com.epita.socra.app.tools.*;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private IOAdapter adapter;
+
+    private App() {
+        this(new ConsoleAdapter());
+    }
+
+    public App(IOAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    /**
+     * Says hello to the world.
+     *
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        App application = new App();
+        application.run();
+    }
+
+    public void run(){
+        adapter.write("Type a number:");
+        String name = adapter.read();
+        while (true) {
+            adapter.write("Nice to meet you, " + name + " !");
+        }
+    }
+}
